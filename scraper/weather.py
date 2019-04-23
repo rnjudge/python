9d37f6f99b056c906a2594de95eb3f67393cab0d
root <root@oc7122357800.ibm.com>
2019-04-22 19:42:10 -0700

    Change gears a litle bit. Write a script that scrapes the weather channel website and gives temp data depending on zip code of users choosing

---
+++ scraper/weather.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+import sys
+from lxml import html
+import requests
+import urllib2
+from bs4 import BeautifulSoup
+
+
+# Collect the site to be scraped from user invocation
+argcheck = len(sys.argv) - 1
+if argcheck < 1:
+        print "You must provide a zipcode to check"
+        exit()
+elif argcheck > 1:
+        print "There are too many zipcodes. Please only list one"
+        exit() # exit for now. 
+	#TODO: collect info from terminal to re-enter only one zipcode instead of exiting
+else:
+        zip = sys.argv[1]
+
+
+page = urllib2.urlopen('https://weather.com/weather/hourbyhour/l/'+zip)
+# soup contains the HTML of the page
+soup = BeautifulSoup(page, 'html.parser')
+
+weather = soup.find("td", attrs={"class": "temp"}).text
+feels = soup.find("td", attrs={"class": "feels"}).text
+city_banner = soup.find("div", attrs={"class": "locations-title"}).text
+# Parse just the location and zip code from the locations-title:
+# We want everything before the ')'
+# TODO figure out how to save ')' from being cut
+location = city_banner.split(")")[0]
+time = soup.find("div", attrs={"class": "observation-timestamp"}).text
+
+print "The current weather in",location + ") at",time + " is",weather + " but feels like",feels
